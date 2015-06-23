b6f779b2094dfddd4cff14f8483c45411a629491
John Hirakawa <johnhirakawa@Johns-MacBook-Pro.local>
2015-06-22 23:54:49 -0400

    Initial Commit

---
+++ AnglerFish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnglerFish
+//
+//  Created by John Hirakawa on 6/22/15.
+//  Copyright (c) 2015 John Hirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
